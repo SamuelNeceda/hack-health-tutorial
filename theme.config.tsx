aa885c1842228ed99a76070dc0908fd209265fd0
Samuel Neceda <samuel.neceda@gmail.com>
2023-03-09 17:37:44 +0100

    customize the page

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,20 +9,27 @@ const config: DocsThemeConfig = {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta property="og:title" content="HackHealth tutorial"/>
             <meta property="og:description" content="Tutorial for the HackHealth app"/>
-            <link rel="icon" href="/public/favicon.png" type="image/png"/>
+            <link rel="icon" href="/favicon.png" type="image/png" sizes="16x16"/>
         </Head>
     ),
-    logo: <span>HackHealth tutorial</span>,
+    faviconGlyph: '👨🏼‍💻',
+    logo: (
+        <>
+            <img src="/logoMain.png" alt="HackHealth tutorial" height="24" width="24" style={{marginRight: "0.4em"}}/>
+            <span>HackHealth tutorial</span>
+        </>
+    ),
     project: {
-        link: 'https://github.com/shuding/nextra-docs-template',
+        link: 'https://git.kpi.fei.tuke.sk/kpi-zp/2023/bp.samuel.neceda/hackHealth',
+        icon: <svg width="24" height="24" viewBox="0 0 256 256">
+            <path fill="currentColor"
+                  d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path>
+        </svg>,
     },
     chat: {
-        link: 'https://discord.com',
+         icon: <svg width="24" height="24" viewBox="0 0 248 204"></svg>,
     },
-    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-    footer: {
-        text: '© 2023 Samuel Neceda. All Rights Reserved.',
-    },
+    docsRepositoryBase: 'https://github.com/SamuelNeceda/hack-health-tutorial',
     useNextSeoProps() {
         const {asPath} = useRouter()
         if (asPath !== '/') {
@@ -30,7 +37,40 @@ const config: DocsThemeConfig = {
                 titleTemplate: 'HH tutorial - %s'
             }
         }
-    }
+    },
+    sidebar: {
+        titleComponent({title}) {
+            if (title === 'Vulnerabilities') {
+                return <>🕵🏼‍♂️ {title}</>
+            }
+            if (title === 'A01:2021 - Broken Access Control') {
+                return <>❕ {title}</>
+            }
+            if (title === 'Introduction') {
+                return <>👋 {title}</>
+            }
+            if (title === 'Initial Setup') {
+                return <>🛠 {title}</>
+            }
+            if (title === 'Web App Setup') {
+                return <>🚀 {title}</>
+            }
+            return <>◽️ {title}</>
+        }
+    },
+    feedback: {
+        content: 'Questions or Suggestions?',
+    },
+    editLink: {
+        text: '',
+    },
+    gitTimestamp: false,
+    footer: {
+        text:
+            <span>
+      MIT {new Date().getFullYear()} © <a href="https://nextra.site" target="_blank">Powered by Nextra template</a>.<br/> Build with ♡ by Samuel Nečeda
+    </span>,
+    },
 }
 
 export default config
