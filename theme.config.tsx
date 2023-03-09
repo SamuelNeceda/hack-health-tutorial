daafc4d08ea4707d629d1a7f20ac57d56e9ac7ba
Samuel Neceda <samuel.neceda@gmail.com>
2023-03-09 12:45:09 +0100

    try to add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
+import {useRouter} from 'next/router'
+import Head from 'next/head'
 
 const config: DocsThemeConfig = {
-  logo: <span>HackHealth tutorial</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+    head: (
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta property="og:title" content="HackHealth tutorial"/>
+            <meta property="og:description" content="Tutorial for the HackHealth app"/>
+            <link rel="icon" href="/public/favicon.png" type="image/png"/>
+        </Head>
+    ),
+    logo: <span>HackHealth tutorial</span>,
+    project: {
+        link: 'https://github.com/shuding/nextra-docs-template',
+    },
+    chat: {
+        link: 'https://discord.com',
+    },
+    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+    footer: {
+        text: '© 2023 Samuel Neceda. All Rights Reserved.',
+    },
+    useNextSeoProps() {
+        const {asPath} = useRouter()
+        if (asPath !== '/') {
+            return {
+                titleTemplate: 'HH tutorial - %s'
+            }
+        }
+    }
 }
 
 export default config
